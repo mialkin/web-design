b73299f5b2b350bc4e11885a6b516cb6764b0e39
Aleksei Mialkin <aleksei@mialkin.ru>
2025-12-21 20:17:20 +0300

    Open and close overlay

--- silde-panel/script.js
+++ silde-panel/script.js
@@ -1,11 +1,26 @@
 (() => {
 
     const button = document.getElementById('button');
+    const overlay = document.getElementById('overlay');
 
     function openPanel() {
-        alert("Opened panel")
+        overlay.classList.add('open');
+        document.addEventListener('keydown', onKeyDown);
+    }
+
+    function closePanel() {
+        overlay.classList.remove('open');
+        document.removeEventListener('keydown', onKeyDown);
+    }
+
+    function onKeyDown(e) {
+        console.log('fire')
+        if (e.key === 'Escape') {
+            closePanel();
+        }
     }
 
     button.addEventListener('click', openPanel);
+    overlay.addEventListener('click', () => closePanel());
 
 })();
