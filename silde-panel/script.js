78d826b435e6700abee59a64d2b8bd66e1306112
Aleksei Mialkin <aleksei@mialkin.ru>
2025-12-21 20:59:59 +0300

    Add sliding panel

--- silde-panel/script.js
+++ silde-panel/script.js
@@ -2,14 +2,17 @@
 
     const button = document.getElementById('button');
     const overlay = document.getElementById('overlay');
+    const panel = document.getElementById('panel');
 
     function openPanel() {
         overlay.classList.add('open');
+        panel.classList.add('open');
         document.addEventListener('keydown', onKeyDown);
     }
 
     function closePanel() {
         overlay.classList.remove('open');
+        panel.classList.remove('open');
         document.removeEventListener('keydown', onKeyDown);
     }
 
