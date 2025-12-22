d78de96f8da57833a27b48364f11fb23e8c24eda
Aleksei Mialkin <aleksei@mialkin.ru>
2025-12-23 00:44:51 +0300

    Add body.no-scroll

--- silde-panel/script.js
+++ silde-panel/script.js
@@ -1,6 +1,7 @@
 (() => {
 
     const button = document.getElementById('button');
+    const closeButton = document.getElementById('close');
     const overlay = document.getElementById('overlay');
     const panel = document.getElementById('panel');
 
@@ -8,12 +9,14 @@
         overlay.classList.add('open');
         panel.classList.add('open');
         document.addEventListener('keydown', onKeyDown);
+        document.body.classList.add('no-scroll');
     }
 
     function closePanel() {
         overlay.classList.remove('open');
         panel.classList.remove('open');
         document.removeEventListener('keydown', onKeyDown);
+        document.body.classList.remove('no-scroll');
     }
 
     function onKeyDown(e) {
@@ -25,5 +28,6 @@
 
     button.addEventListener('click', openPanel);
     overlay.addEventListener('click', () => closePanel());
+    closeButton.addEventListener('click', () => closePanel());
 
 })();
